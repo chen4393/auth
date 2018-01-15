e110bdb12bd9fb0a8459227f4138be0ee83ec832
Macbook <Macbook@Gazelles-MacBook-3.local>
2018-01-15 13:09:22 -0600

    Added Authentication and Spinner

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
   buttonStyle: {
     flex: 1,
     alignSelf: 'stretch',
-    backgroundColor: '#ffff00',
+    backgroundColor: '#fff',
     borderRadius: 5,
     borderWidth: 1,
     borderColor: '#007aff',
